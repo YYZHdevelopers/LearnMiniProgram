2705d8ff356fc8831bdd2c7a872bb9f567593535
YYZHdevelopers <834183817@qq.com>
2020-02-14 14:07:16 +0800

    小程序的初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      name:"杨一哲",
+      age:"18",
+      students:[
+      {id:110,name:'kobe',age:30},
+      {id:111,name:'james',age:28},
+      {id:112,name:'curry',age:32},
+      {id:113,name:'YYZH',age:20}
+      ],
+      counter:0
   },
+  handadd(){
+    // 错误做法
+    // this.data.counter+=1
+    // console.log(this.data.counter)
 
+    // 正确做法：this.setData()
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  handsub(){
+
+    this.setData({
+      counter:this.data.counter-1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
