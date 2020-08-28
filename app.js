6317276f07fa96d7ab4633129ec794285b16d73c
YYZHdevelopers <834183817@qq.com>
2020-08-28 23:02:54 +0800

    注册page时做什么

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ App({
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    
+  onLaunch: function() {
+
   },
 
   /**
@@ -13,10 +13,10 @@ App({
    * 
    * 会被多次触发
    */
-  onShow: function (options) {
+  onShow: function(options) {
     // 1、判断小程序的进入场景
     console.log(options);
-    switch(options.scene){
+    switch (options.scene) {
       case 1001:
         break;
       case 1005:
@@ -26,29 +26,29 @@ App({
     // 获取用户信息的第一种方式
     wx.getUserInfo({
       success: function(res) {
-        console.log(res);
+        console.log(res.rawData);
       },
-      
+
     })
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
-  onError: function (msg) {
-    
+  onError: function(msg) {
+
   },
 
-  globalDate:{
-    Admin_name:"YYZH",
-    Admin_age:"20",
-    Admin_hobby:"conding"
+  globalDate: {
+    Admin_name: "YYZH",
+    Admin_age: "20",
+    Admin_hobby: "conding"
   }
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -6,16 +6,62 @@ var name=app.globalDate.Admin_name;
 
 
 Page({
-  
-
-  handelGetuserInfo(event){
-    console.log(event);
-  },
-
   /**
    * 页面的初始数据
    */
+   // ----------------------2、初始化数据--------------------
   data: {
     name: name,
+  },
+
+  
+
+  // ----------------------1、监听生命函数--------------------
+  // 1、页面被加载出来时
+  onLoad:function(){
+    console.log("onLoad"); 
+  },
+  // 2、页面初次渲染完成时
+  onReady: function (){
+    console.log("onReady");
+  },
+  // 3、页面显示出来时
+  onShow: function (){
+    console.log("onShow");
+  },
+  // 4、页面被隐藏时
+  onHide: function (){
+    console.log("onHide");
+  },
+  // 5、页面被切换掉时
+  onUnload: function (){
+
+  },
+   // ----------------------3、监听相关事件--------------------
+  handelGetuserInfo(event) {
+    console.log(event);
+  },
+  handelViewClick(){
+    console.log("name被点击");
+  },
+
+   // ----------------------4、监听其他事件--------------------
+
+  // 监听页面滚动
+  onPageScroll(obj){
+
+    // console.log(obj);
+  },
+  // 监听页面滚动到底部
+  onReachBottom(){
+    console.log("页面到底了~");
+  },
+  // 监听用户下拉刷新
+  onPullDownRefresh(){
+    console.log("正在刷新~");
+  },
+  // 监听tab触发
+  onTabItemTap(item){
+    console.log(item.pagePath);
   }
 })
