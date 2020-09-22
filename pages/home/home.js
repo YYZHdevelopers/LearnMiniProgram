d75834ad3644b76e4c12759dda9b111bc5637040
YYZHdevelopers <834183817@qq.com>
2020-09-22 16:54:00 +0800

    网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,8 @@
 // pages/home/home.js
+import request from "../server/server"
 Page({
 
+  
   /**
    * 页面的初始数据
    */
@@ -12,55 +14,53 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 1、原始的网络请求方法
+    // this.get_origin_request();
+
+    // 2、封装工具函数的网络请求方法
+    request({
+      url:'http://152.136.185.210:8000/api/z8/recommend'
+    }).then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+    })
 
   },
+  get_origin_request(){
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+     // 网络请求
+    // wx.request({
+    //   // 1、普通网络请求
+    //   url: 'http://httpbin.org',
+    //   success:function(res){
+    //       console.log(res);
+    //   },
+      
+    // })
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method:'post',
+      data:{
+        name:"yyzh",
+        age:"20"
+      },
+      success:function(res){
+        console.log(res);
+      },
+      fail:function(err){
+        console.log(err);
+      }
+    }),
+    wx.request({
+      url: 'http://152.136.185.210:8000/api/z8/recommend',
+      success:function(res){
+        console.log(res);
+      }
+    })
   }
+
+
+
+  
 })
---
+++ pages/server/server.js
@@ -0,0 +1,20 @@
+// 工具函数封装
+export default function requset(options){
+    return   new Promise((resolve,reject)=>{
+      wx.request({
+        url: options.url,
+        method:options.method||"get",
+        data:options.data || {},
+        success:function(res){
+          resolve(res)
+        },
+        fail:function(err){
+          reject(err)
+        }
+      })
+
+
+
+    })
+
+}
