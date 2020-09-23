f7f73f8f9f87cef4e8bf992b0214403fc368d012
YYZHdevelopers <834183817@qq.com>
2020-09-23 14:45:37 +0800

    分享和弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,67 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handelShowtoast(){
+    wx.showToast({
+      title: '添加成功!',
+      mask:true,
+      image:'/pages/ico/add-circle.png',
+      duration:3000,
+      success:function(res){
+        console.log("调用成功");
+      },
+      fail:function(err){
+        console.log("调用失败");
+      },
+      complete:function(){
+        console.log("调用结束");
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handelShowmodal(){
+    wx.showModal({
+      title:'请确认地址',
+      content:'xx省xx市xxx县xx镇',
+      success(res){
+        if(res.cancel){
+          console.log('用户点击了取消');
+        }
+        if(res.confirm){
+          console.log('用户点击了确认');
+        }
+      },
+      cancelText:'不是',
+      cancelColor:'red'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handelShowloading(){
+    wx.showLoading({
+      title: 'loading',
+      mask:true
+    })
   },
+  handelShowAction(){
+    wx.showActionSheet({
+      itemList: ["拍照",'从相册中选择一张'],
+      itemColor:'red',
+      success:function(res){
+       if(res.tapIndex==0){
+        // 打开相机
+       }
+       if(res.tapIndex==1){
+        wx.chooseImage({
+          count: 9,
+        })
+       }
+      }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 用户分享
+  onShareAppMessage: function (options) {
+    return {
+        title:'这是第一次分享',
+        path:'/pages/about/about.wxml',
+        imageUrl:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600792624826&di=532d24bfa7f0bc411c78dbd15dd628b6&imgtype=0&src=http%3A%2F%2Ft7.baidu.com%2Fit%2Fu%3D3616242789%2C1098670747%26fm%3D79%26app%3D86%26f%3DJPEG%3Fw%3D900%26h%3D1350'
+    }
   }
 })
